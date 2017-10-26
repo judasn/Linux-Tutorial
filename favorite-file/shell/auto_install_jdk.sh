c8796468ed2075b46431cb5271b1a05ed1ad5a33
zhangchaohuang <zhangchaohuang@banggood.com>
2017-10-26 17:51:33 +0800

    2017-10-26 完善脚本细节

--- favorite-file/shell/auto_install_jdk.sh
+++ favorite-file/shell/auto_install_jdk.sh
@@ -13,27 +13,27 @@ fi
 echo "下载 JDK"
 
 cd /opt/setups
-wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
 
-if [ ! -f "/opt/setups/jdk-8u144-linux-x64.tar.gz" ]; then
+if [ ! -f "/opt/setups/jdk-8u151-linux-x64.tar.gz" ]; then
 	echo "JDK 下载失败，结束脚本"
 	exit 1
 fi
 
 echo "JDK 下载成功，开始解压 JDK"
-tar -zxf jdk-8u144-linux-x64.tar.gz
+tar -zxf jdk-8u151-linux-x64.tar.gz
 
-if [ ! -d "/opt/setups/jdk1.8.0_144" ]; then
+if [ ! -d "/opt/setups/jdk1.8.0_151" ]; then
 	echo "JDK 解压失败，结束脚本"
 	exit 1
 fi
 
 echo "JDK 解压包移到 /usr/program/ 目录下"
-mv jdk1.8.0_144/ /usr/program/
+mv jdk1.8.0_151/ /usr/program/
 
 echo "JDK 写入系统变量到 zshrc"
 
-echo 'JAVA_HOME=/usr/program/jdk1.8.0_144' >> ~/.zshrc  
+echo 'JAVA_HOME=/usr/program/jdk1.8.0_151' >> ~/.zshrc  
 echo 'JRE_HOME=$JAVA_HOME/jre' >> ~/.zshrc  
 echo 'PATH=$PATH:$JAVA_HOME/bin' >> ~/.zshrc  
 echo 'CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar' >> ~/.zshrc  
--- favorite-file/shell/auto_install_tomcat_centos6.sh
+++ favorite-file/shell/auto_install_tomcat_centos6.sh
@@ -13,24 +13,24 @@ fi
 echo "下载 Tomcat"
 
 cd /opt/setups
-wget http://apache.fayea.com/tomcat/tomcat-8/v8.0.46/bin/apache-tomcat-8.0.46.tar.gz
+wget http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz
 
-if [ ! -f "/opt/setups/apache-tomcat-8.0.46.tar.gz" ]; then
+if [ ! -f "/opt/setups/apache-tomcat-8.0.47.tar.gz" ]; then
 	echo "Tomcat 下载失败，结束脚本"
 	exit 1
 fi
 
 echo "Tomcat 下载成功，开始解压 Tomcat"
-tar -zxf apache-tomcat-8.0.46.tar.gz
+tar -zxf apache-tomcat-8.0.47.tar.gz
 
-if [ ! -d "/opt/setups/apache-tomcat-8.0.46" ]; then
+if [ ! -d "/opt/setups/apache-tomcat-8.0.47" ]; then
 	echo "Tomcat 解压失败，结束脚本"
 	exit 1
 fi
 
 echo "Tomcat 解压包移到 /usr/program/ 目录下"
-mv apache-tomcat-8.0.46/ /usr/program/
-mv /usr/program/apache-tomcat-8.0.46/ /usr/program/tomcat8/
+mv apache-tomcat-8.0.47/ /usr/program/
+mv /usr/program/apache-tomcat-8.0.47/ /usr/program/tomcat8/
 
 echo "防火墙放行 8080 端口"
 iptables -I INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
