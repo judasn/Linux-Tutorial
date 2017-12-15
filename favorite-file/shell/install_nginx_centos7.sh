ae99ac8bccffa7ee694eb0b5291c809649738758
jn3.141592654@gmail.com <jn3.141592654@gmail.com>
2017-12-15 10:00:08 +0800

    2017-12-15 nginx 脚本版本更新

--- favorite-file/shell/install_nginx_centos7.sh
+++ favorite-file/shell/install_nginx_centos7.sh
@@ -31,17 +31,17 @@ fi
 echo "下载 Nginx"
 
 cd /opt/setups
-wget http://nginx.org/download/nginx-1.12.1.tar.gz
+wget https://nginx.org/download/nginx-1.12.2.tar.gz
 
-if [ ! -f "/opt/setups/nginx-1.12.1.tar.gz" ]; then
+if [ ! -f "/opt/setups/nginx-1.12.2.tar.gz" ]; then
 	echo "Nginx 下载失败，结束脚本"
 	exit 1
 fi
 
 echo "Nginx 下载成功，开始解压 Nginx"
-tar -zxf nginx-1.12.1.tar.gz
+tar -zxf nginx-1.12.2.tar.gz
 
-if [ ! -d "/opt/setups/nginx-1.12.1" ]; then
+if [ ! -d "/opt/setups/nginx-1.12.2" ]; then
 	echo "Nginx 解压失败，结束脚本"
 	exit 1
 fi
@@ -50,7 +50,7 @@ echo "安装源码安装依赖"
 yum install -y gcc gcc-c++ pcre pcre-devel zlib zlib-devel openssl openssl-devel
 
 echo "开始安装 Nginx"
-cd nginx-1.12.1/
+cd nginx-1.12.2/
 
 ./configure --prefix=/usr/local/nginx --pid-path=/var/local/nginx/nginx.pid --lock-path=/var/lock/nginx/nginx.lock --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-http_gzip_static_module --http-client-body-temp-path=/var/temp/nginx/client --http-proxy-temp-path=/var/temp/nginx/proxy --http-fastcgi-temp-path=/var/temp/nginx/fastcgi --http-uwsgi-temp-path=/var/temp/nginx/uwsgi --with-http_ssl_module --http-scgi-temp-path=/var/temp/nginx/scgi
 make
