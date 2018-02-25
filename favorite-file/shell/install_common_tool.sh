20e12004fd21f66d989078f13120f948795a69f5
youmeek <jn3.141592654@gmail.com>
2018-02-25 10:24:39 +0800

    2018-02-25

--- favorite-file/shell/install_common_tool.sh
+++ favorite-file/shell/install_common_tool.sh
@@ -2,6 +2,6 @@
 
 echo "开始常用工具安装"
 
-yum install -y zip unzip lrzsz git
+yum install -y zip unzip lrzsz git epel-release
 
 echo "开始常用工具结束"
--- favorite-file/shell/install_vim_zsh.sh
+++ favorite-file/shell/install_vim_zsh.sh
@@ -2,7 +2,7 @@
 
 echo "开始常用工具安装"
 
-yum install -y zip unzip lrzsz git
+yum install -y zip unzip lrzsz git epel-release
 
 echo "开始常用工具结束"
 
